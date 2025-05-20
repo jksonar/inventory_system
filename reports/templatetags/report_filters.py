a2f466120c17d3e8ff3a63cb5e0184804b572283
j.sonar <j.sonar@direction.biz>
2025-05-20 19:16:33 +0530

    inventory report updated

---
+++ reports/templatetags/report_filters.py
@@ -0,0 +1,14 @@
+from django import template
+from decimal import Decimal
+
+register = template.Library()
+
+@register.filter(name='multiply')
+def multiply(value, arg):
+    """
+    Multiplies the value by the argument
+    """
+    try:
+        return Decimal(str(value)) * Decimal(str(arg))
+    except (ValueError, TypeError):
+        return 0
